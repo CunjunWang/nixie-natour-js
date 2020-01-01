ab3ca7c8735623df5df3883192c3391c5430af28
Cunjun Wang <duckwcj@gmail.com>
2020-01-01 13:56:27 -0500

    config different env

--- nixie-natour-node/app.js
+++ nixie-natour-node/app.js
@@ -11,9 +11,11 @@ const app = express();
 // ================== MIDDLE-WARES ===================
 // the order of middleware matters
 // all routes are also middleware in express
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development')
+    app.use(morgan('dev'));
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware');
