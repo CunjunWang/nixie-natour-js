034dc9d90c90bf42a18bed4bd701ff52d867354e
Cunjun Wang <duckwcj@gmail.com>
2020-01-07 13:32:08 +0800

    connect to mongo db

--- nixie-natour-node/server.js
+++ nixie-natour-node/server.js
@@ -1,9 +1,25 @@
 // Created by CunjunWang on 2020/1/1
 
+const mongoose = require('mongoose');
+
 const env = require('dotenv');
 env.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE_URL
+  .replace('<USERNAME>', process.env.DATABASE_USERNAME)
+  .replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+  .replace('<HOST>', process.env.DATABASE_HOST)
+  .replace('<DATABASE>', process.env.DATABSE);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => {
+  console.log('DB connection successful');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
