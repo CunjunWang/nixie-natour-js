91a1e4a24e0e68a29f27144b602834b06e1e6ee2
Cunjun Wang <duckwcj@gmail.com>
2020-01-08 15:45:37 +0800

    get documents from database

--- nixie-natour-node/controllers/tourController.js
+++ nixie-natour-node/controllers/tourController.js
@@ -2,27 +2,40 @@
 
 const Tour = require('./../models/tourModels');
 
-exports.getAllTours = (req, res) => {
-  // res.status(200).json({
-  //   status: 'success',
-  //   results: tours.length,
-  //   data: {
-  //     tours
-  //   }
-  // });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.getTourWithID = (req, res) => {
-  // ':id?' to make it optional
-  // const id = req.params.id * 1;
-  // const tour = tours.find(el => el.id === id);
-  //
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour
-  //   }
-  // });
+exports.getTourWithID = async (req, res) => {
+  try {
+    // ':id?' to make it optional;
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
