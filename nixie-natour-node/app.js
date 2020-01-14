fa355afd3a2e8b663f88a6528083390d886a2c01
Cunjun Wang <duckwcj@gmail.com>
2020-01-14 23:05:31 +0800

    prevent mongo injection, xss, and parameter pollution

--- nixie-natour-node/app.js
+++ nixie-natour-node/app.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -14,20 +18,45 @@ const app = express();
 // ================== Global MIDDLE-WARES ===================
 // the order of middleware matters
 // all routes are also middleware in express
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development environment setting
 if (process.env.NODE_ENV === 'development')
   app.use(morgan('dev'));
 
-// rate limiter
+// Rate limiter
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many request from this IP. Please try again later'
 });
-
 app.use('/api', limiter);
-app.use(express.json());
+
+// Body parser, reading data from body into req.body
+app.use(express.json({
+  limit: '10kb'
+}));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+  whitelist: [
+    'duration', 'ratingsQuantity', 'ratingsAverage',
+    'maxGroupSize', 'difficulty', 'price'
+  ]
+}));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
