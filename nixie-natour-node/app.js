fa98bfb76d6df731ab23d464b5d2be46171f01d8
Cunjun Wang <duckwcj@gmail.com>
2020-01-14 22:39:58 +0800

    send jwt with cookie; add rate limiter

--- nixie-natour-node/app.js
+++ nixie-natour-node/app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,12 +11,20 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// ================== MIDDLE-WARES ===================
+// ================== Global MIDDLE-WARES ===================
 // the order of middleware matters
 // all routes are also middleware in express
 if (process.env.NODE_ENV === 'development')
   app.use(morgan('dev'));
 
+// rate limiter
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP. Please try again later'
+});
+
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
