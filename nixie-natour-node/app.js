deac526e85df84e634eeaf855310dbf532e8cfc2
Cunjun Wang <duckwcj@gmail.com>
2020-01-15 23:32:12 +0800

    implement basic review resources

--- nixie-natour-node/app.js
+++ nixie-natour-node/app.js
@@ -12,6 +12,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -66,6 +67,7 @@ app.use((req, res, next) => {
 // =================== ROUTING ====================
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // handle unmatched router
 app.all('*', (req, res, next) => {
