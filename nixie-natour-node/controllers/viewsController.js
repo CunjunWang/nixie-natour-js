2cf429d4a9e2b3cf7b31229a56f16ff1706670e7
Cunjun Wang <duckwcj@gmail.com>
2020-01-17 16:25:51 +0800

    bulid the overview page

--- nixie-natour-node/controllers/viewsController.js
+++ nixie-natour-node/controllers/viewsController.js
@@ -1,10 +1,19 @@
 // Created by CunjunWang on 2020/1/17
 
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1. Get tour data from the collection
+  const tours = await Tour.find();
+
+  // 2. build template
+  // 3. render that template using tour data
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
