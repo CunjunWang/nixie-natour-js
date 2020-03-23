d4141b54ce3578f36f7e9cae31a8e98eb342d25c
Cunjun Wang <duckwcj@gmail.com>
2020-03-23 17:19:09 -0400

    prepared for deploy

--- nixie-natour-node/public/js/stripe.js
+++ nixie-natour-node/public/js/stripe.js
@@ -8,7 +8,7 @@ const stripe = Stripe('pk_test_ikugjEdqbtVReAl7LWPq9HPA001XQeixo4');
 export const bookTour = async tourId => {
   try {
     // 1. get the session from the server
-    const session = await axios(`http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 
     // 2. create checkout form + charge the credit card
     await stripe.redirectToCheckout({ sessionId: session.data.session.id });
