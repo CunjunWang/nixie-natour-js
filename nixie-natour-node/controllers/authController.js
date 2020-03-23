c3ac67c9f6f10fa9e5c66ba8e91c87d64730f94a
Cunjun Wang <duckwcj@gmail.com>
2020-03-23 12:49:22 -0400

    add emails

--- nixie-natour-node/controllers/authController.js
+++ nixie-natour-node/controllers/authController.js
@@ -6,7 +6,7 @@ const crypto = require('crypto');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -46,6 +46,10 @@ exports.signUp = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+
+  await new Email(newUser, url).sendWelcome();
 
   createAndSendToken(newUser, 201, res);
 });
@@ -140,11 +144,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit your new password and confirm to: ${resetUrl}\nIf you did not forget your password, ignore this message.`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 minutes)',
-      message
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 minutes)',
+    //   message
+    // });
 
     res.status(200).json({
       status: 'success',
--- nixie-natour-node/utils/email.js
+++ nixie-natour-node/utils/email.js
@@ -9,7 +9,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Cunjun Wang <${process.env.EMAIL_FROM}>`;
+    this.from = `Natours Admin <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
