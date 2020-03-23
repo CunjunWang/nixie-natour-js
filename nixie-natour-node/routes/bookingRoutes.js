12a5e2b06a2664f423825668babeb50909e40e81
Cunjun Wang <duckwcj@gmail.com>
2020-03-23 17:01:09 -0400

    finish all apis

--- nixie-natour-node/routes/bookingRoutes.js
+++ nixie-natour-node/routes/bookingRoutes.js
@@ -8,8 +8,19 @@ const router = express.Router({
   mergeParams: true
 });
 
+router.use(authController.protect);
+
 router.get('/checkout-session/:tourId',
-  authController.protect,
   bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router.route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router.route('/:id').get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
